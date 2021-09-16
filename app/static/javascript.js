8e4939c0d35bd7547869c0157938915033a08a86
lucasrosa14 <contact.lucassr@gmail.com>
2021-09-16 12:28:26 -0300

    correção projetc

--- app/static/javascript.js
+++ app/static/javascript.js
@@ -25,19 +25,15 @@
             let ajax = new XMLHttpRequest();
             let token = doc.querySelectorAll('input')[0].value;
             ajax.open('POST', form.action);
-            console.log('1')
             ajax.setRequestHeader('X-CSRF-TOKEN',token);
             ajax.onreadystatechange = function()
             {
                 if(ajax.status === 200 && ajax.readyState === 4){
-                    console.log('2')
                     let result = doc.querySelector('#result');
                     result.innerHTML = 'Operação realizada com sucesso!';
-                    console.log('3')
                     result.classList.add('alert');
-                    console.log('4')
                     result.classList.add('alert-success');
-                    console.log('5')
+
                 }
             }
             ajax.send(data);
